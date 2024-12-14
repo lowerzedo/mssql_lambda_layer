70c35be707b970856a3c38b6891658edc29dbe39
Nazar Hasanov <tp052475@mail.apu.edu.my>
2024-12-14 10:17:08 +0800

    fix: improved script

--- create-mssql-layer.sh
+++ create-mssql-layer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Create working directory
+mkdir -p mssql-lambda-build
+cd mssql-lambda-build
+
 # Create a directory structure for the layer
-mkdir -p mssql-layer/python/lib/python3.10/site-packages
+mkdir -p python/lib/python3.10/site-packages
 
 # Create a requirements.txt file
 cat > requirements.txt << EOL
@@ -20,31 +24,33 @@ RUN yum update -y && \
 RUN curl https://packages.microsoft.com/config/rhel/8/prod.repo > /etc/yum.repos.d/mssql-release.repo && \
     ACCEPT_EULA=Y yum install -y msodbcsql18
 
+WORKDIR /layer
+
 # Copy requirements
 COPY requirements.txt .
 
 # Install Python packages
-RUN pip install -r requirements.txt -t /python/lib/python3.10/site-packages/
+RUN pip install -r requirements.txt -t python/lib/python3.10/site-packages/
 
 # Copy the ODBC drivers and their dependencies
-RUN cp -r /opt/microsoft /python/ && \
-    cp -r /usr/lib64/libmsodbcsql* /python/lib/ && \
-    cp -r /usr/lib64/libodbccr* /python/lib/ && \
-    cp -r /usr/lib64/libodbcinst* /python/lib/
+RUN mkdir -p python/lib && \
+    cp -r /opt/microsoft python/ && \
+    cp -r /usr/lib64/libmsodbcsql* python/lib/ && \
+    cp -r /usr/lib64/libodbccr* python/lib/ && \
+    cp -r /usr/lib64/libodbcinst* python/lib/
 EOL
 
 # Build the Docker image
 docker build -t mssql-layer .
 
 # Create a container and copy the layer contents
-docker create --name temp_container mssql-layer
-docker cp temp_container:/python/. mssql-layer/python/
+docker create --name temp_container mssql-layer bash
+docker cp temp_container:/layer/python .
 docker rm temp_container
 
 # Create the ZIP file for the Lambda layer
-cd mssql-layer
-zip -r ../mssql-layer.zip .
-cd ..
+zip -r ../mssql-lambda-layer.zip python/
 
 # Clean up
-rm -rf mssql-layer
+cd ..
+rm -rf mssql-lambda-build
